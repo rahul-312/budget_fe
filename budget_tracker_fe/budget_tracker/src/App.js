00111bc45112ba8021769263d2dacdf8de240d8b
Siddappanor Rahul <138891259+rahul-312@users.noreply.github.com>
2025-05-12 11:33:02 +0530

    Update App.js

--- budget_tracker_fe/budget_tracker/src/App.js
+++ budget_tracker_fe/budget_tracker/src/App.js
@@ -1,30 +1,43 @@
-// src/App.js
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import Layout from "./components/Layout";
-
-import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Transactions from "./pages/Transactions/Transactions";
 import EditTransactionPage from "./pages/Transactions/EditTransactionPage";
 import Budgets from "./pages/Budgets/Budgets";
 import Dashboard from "./pages/Dashboard/Dashboard";
-
 import "font-awesome/css/font-awesome.min.css";
 import "./styles/App.css";
+
+// Function to check if user is authenticated
+const isAuthenticated = () => {
+  return !!localStorage.getItem("access_token");
+};
+
+// Wrapper to protect private routes
+const PrivateRoute = () => {
+  return isAuthenticated() ? <Outlet /> : <Navigate to="/login" replace />;
+};
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          {/* Redirect root to login */}
+          <Route index element={<Navigate to="/login" replace />} />
+
+          {/* Public Routes */}
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="transactions" element={<Transactions />} />
-          <Route path="transactions/:id/edit" element={<EditTransactionPage />} />
-          <Route path="budgets" element={<Budgets />} />
+
+          {/* Private Routes */}
+          <Route element={<PrivateRoute />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="transactions" element={<Transactions />} />
+            <Route path="transactions/:id/edit" element={<EditTransactionPage />} />
+            <Route path="budgets" element={<Budgets />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
